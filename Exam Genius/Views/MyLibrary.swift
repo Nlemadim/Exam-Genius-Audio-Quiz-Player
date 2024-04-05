13b7cc77f1606c77a17a0683b43719b513a2e178
Nlemadim <nlemadimtony@gmail.com>
2024-04-05 14:54:35 -0400

    Updated from stash

--- Exam Genius/Views/MyLibrary.swift
+++ Exam Genius/Views/MyLibrary.swift
@@ -7,40 +7,18 @@
 
 import SwiftUI
 import SwiftData
-import Combine
-import AVKit
 
 struct MyLibrary: View {
     @Environment(\.modelContext) private var modelContext
     @Environment(\.dismiss) private var dismiss
     @EnvironmentObject var user: User
     @EnvironmentObject var appState: AppState
-    
     @StateObject var viewModel = MyLibrary.MyLibraryVM()
-    @StateObject private var generator = ColorGenerator()
-    @StateObject var questionPlayer = QuestionPlayer()
-    @StateObject var responseListener = ResponseListener()
-    @StateObject var quizSetter = QuizPlayerView.QuizSetter()
     
-    @State private var currentQuestion: Question?
-    @State var selectedQuizPackage: AudioQuizPackage?
-    @State var configuration: QuizViewConfiguration?
-    @State var audioPlaylist: [PlayerContent] = []
-    @State var interactionState: InteractionState = .idle
-    
-    
-    @State private var playTapped: Bool = false
-    @State private var nextTapped: Bool = false
-    @State private var repeatTapped: Bool = false
-    @State private var presentMicModal: Bool = false
+    @State private var selectedQuizPackage: AudioQuizPackage?
+    @State private var interactionState: InteractionState = .idle
     @State private var isDownloaded: Bool = false
-    @State private var expandSheet: Bool = false
-    @State var isDownloading: Bool = false
-    @State var isPlaying: Bool = false
-    @State var presentConfirmationModal: Bool = false
-    
-    @State var currentQuestionIndex: Int = 0
-    @State var selectedOption: String = ""
+    @State var audioPlaylist: [PlayerContent] = []
     
     
     var body: some View {
@@ -63,78 +41,37 @@ struct MyLibrary: View {
                 ScrollView {
                     VStack(alignment: .leading, spacing: 0) {
                         ForEach(audioPlaylist, id: \.self) { content in
-                            PlayerContentItemView(content: content, interactionState: $interactionState, isDownloaded: $isDownloaded)
-                                .onTapGesture {
-                                    self.expandSheet.toggle()
-                                }
-                            
+                            PlayerContentItemView(content: content, playContent: {}, interactionState: $interactionState, isDownloaded: $isDownloaded)
                             Divider().padding()
                         }
                     }
                 }
                 .onAppear {
-                    updatePlaylist()
-                }
-                .onAppear {
-                    if let audioQuiz = selectedQuizPackage {
-                        quizSetter.loadQuizConfiguration(quizPackage: audioQuiz)
+                    if let quizPackage = user.audioQuizPackage {
+                        viewModel.addAudioQuizToLibrary(from: quizPackage)
+                        print(quizPackage.questions.count)
                     }
-                }
-                .onChange(of: selectedQuizPackage) { _, newValue in
-                    if let newPackage = newValue {
-                        quizSetter.loadQuizConfiguration(quizPackage: newPackage)
-                    }
-                }
-                .onChange(of: playTapped, { _, _ in
-                    playQuestion()
-                })
-                .onChange(of: nextTapped) { _, _ in
-                    goToNextQuestion()
-                }
-                .onChange(of: questionPlayer.interactionState) { _, newState in
-                    checkPlayerState(newState)
-                }
-                .onChange(of: responseListener.interactionState) { _, interaction in
-                    checkForResponse(interaction)
+                    
+                    viewModel.loadMockData()
                 }
                 .onChange(of: user.hasSelectedAudioQuiz) {_, _ in
-                    updatePlaylist()
+                    if let quizPackage = user.audioQuizPackage {
+                        viewModel.addAudioQuizToLibrary(from: quizPackage)
+                        
+                    }
                 }
-                .fullScreenCover(isPresented: $expandSheet) {
-                    QuizView(quizSetter: quizSetter, currentQuestionIndex: $currentQuestionIndex, isNowPlaying: $playTapped, isCorrectAnswer: $presentConfirmationModal, presentMicModal: $presentMicModal, nextTapped: $nextTapped, interactionState: $interactionState)
-                }
-               
+                .onReceive(viewModel.$audioPlaylist, perform: { playlist in
+                    DispatchQueue.main.async {
+                        self.audioPlaylist.append(contentsOf: playlist)
+                        print(audioPlaylist.count)
+                    }
+                })
                 
                 Spacer()
             }
         }
         .preferredColorScheme(.dark)
     }
-    
-    
-    private func checkForResponse(_ interaction: InteractionState) {
-        self.interactionState = interaction
-        DispatchQueue.main.async {
-            //self.interactionState = newValue
-            print("QuizPlayer interaction State is: \(self.interactionState)")
-            if interaction == .idle {
-                self.selectedOption = responseListener.userTranscript
-                print("Quiz view has registered new selectedOption as: \(self.selectedOption)")
-                analyseResponse()
-            }
-        }
-        
-    }
-    
-    private func checkPlayerState(_ interaction: InteractionState) {
-        self.interactionState = interaction
-        if interaction == .isDonePlaying {
-            print("QuizPlayer interaction State is: \(self.interactionState)")
-            responseListener.recordAnswer()
-        }
-        
-    }
-    
 }
 
 #Preview {
